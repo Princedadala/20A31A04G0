059eb1ee4195fb171a3a20db158a1f2cf0f688fb
Princedadala <113782068+Princedadala@users.noreply.github.com>
2023-02-04 15:00:51 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+class number:
+    even= 0
+    def check(self,num):
+        if num%2==0:
+            self.even=1
+    def even_odd(self,num):
+        self.check(num)
+        if self.even==1:
+            print(num,"is even")
+        else:
+            print(num,"is odd")
+num=number()
+num.even_odd(int(input("enter number")))
